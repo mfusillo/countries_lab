8fa10ac3854d6b25f0857ecf2ac2883c868179e6
Matteo Fusillo <matt.fusillo@gmail.com>
2020-01-14 15:24:49 +0000

    add fetch function and further testing

--- src/app.js
+++ src/app.js
@@ -1,5 +1,21 @@
 import Vue from 'vue';
 
 document.addEventListener('DOMContentLoaded', () => {
-  console.log("DOM Has Loaded");
-})
+  new Vue({
+    el: '#app',
+    data: {
+      countryURL: null,
+      countries: []
+    },
+    mounted(){
+      this.fetchListAllCountries();
+    },
+    methods: {
+      fetchListAllCountries: function(){
+        fetch('https://restcountries.eu/rest/v2/all')
+        .then(response => response.json())
+        .then(countries => this.countries = countries);
+      }
+    }
+  });
+});
